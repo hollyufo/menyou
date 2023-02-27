05af216ae143ea7ec7d675365efc0b8bb2c2c26e
imrane chaibi <54860557+hollyufo@users.noreply.github.com>
2023-02-27 10:59:26 +0100

    added menu

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -3,6 +3,7 @@ import 'package:menyou/constants/colors.dart';
 
 import '../../Widgets/CNavbar.dart';
 import '../../Widgets/MyBottomNavBar.dart';
+import '../../Widgets/foodmenue.dart';
 
 class HomePage extends StatefulWidget {
 
@@ -13,7 +14,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: kBackgroundColor,
       // getting the navbar from the widgets folder as a top navigation bar
       appBar: PreferredSize(
@@ -21,43 +22,7 @@ class _HomePageState extends State<HomePage> {
         child: CNavbar(Icons.home, Icons.search),
       ),
       // list of cards in the body
-      body: Column(
-            children: [
-              Card(
-                child: ListTile(
-                  title: Text('Card 1'),
-                  subtitle: Text('This is the first card'),
-                  leading: Icon(Icons.access_alarm),
-                  trailing: Icon(Icons.arrow_forward),
-                  onTap: () {
-                    // Do something when the user taps on this card
-                  },
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  title: Text('Card 2'),
-                  subtitle: Text('This is the second card'),
-                  leading: Icon(Icons.access_time),
-                  trailing: Icon(Icons.arrow_forward),
-                  onTap: () {
-                    // Do something when the user taps on this card
-                  },
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  title: Text('Card 3'),
-                  subtitle: Text('This is the third card'),
-                  leading: Icon(Icons.accessibility),
-                  trailing: Icon(Icons.arrow_forward),
-                  onTap: () {
-                    // Do something when the user taps on this card
-                  },
-                ),
-              ),
-            ],
-        ),
+      body: FoodMenu(),
       bottomNavigationBar: MyBottomNavBar(),
     );
   }
